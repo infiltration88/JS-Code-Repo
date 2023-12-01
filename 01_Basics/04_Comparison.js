3e244a287a1667e73494b0f7c4fbbe3a80bab957
infiltration88 <133121409+infiltration88@users.noreply.github.com>
2023-12-01 19:16:42 +0000

    Comparision

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">0);
+// console.log("02">0);
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null==0);
+
+// console.log(undefined >0);
+// console.log(undefined ==0);
+// console.log(undefined <0);
+
+console.log("2"===2); // === means strict conversion check it first checks if they are the same data type and then proceeds 
+// to compare the data type
+
